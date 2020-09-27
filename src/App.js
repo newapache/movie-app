f9fec0beee499662100c83df2fbae72fc099dbdf
jang-yunmi <newapache@naver.com>
2020-09-27 23:23:56 +0900

    add css

--- src/App.js
+++ src/App.js
@@ -38,9 +38,29 @@ class App extends React.Component {
 
   render(){
     const { isLoading, movies }  = this.state;
-    return <div>{ isLoading ? "Loading.." : movies.map(movie => {
-      return <Movie key={ movie.id } id={ movie.id } year={ movie.year } title={ movie.title } summary={ movie.summary } poster={ movie.poster }/>
-    })}</div>
+    return (
+        <section className="container">
+          {isLoading ? (
+            <div className="loader">
+              <span className="loader__text">Loading...</span>
+            </div>
+          ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+          </div>
+          )}
+      </section>
+    )
   }
 }
 
