2a48a5a02c71fbec766db486dbd8c60525c6f7da
jang-yunmi <newapache@naver.com>
2020-09-27 22:22:43 +0900

    get movie data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import Movie from './Movies';
 
 // function App() {
 //   return (
@@ -14,16 +16,32 @@ class App extends React.Component {
     isLoading: true,
     movies: []
   };
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 3000);
-  }
-  render(){
-    const { isLoading }  = this.state;
-    return <div>{ isLoading ? "isLoading is true" : "isLoading is false"}</div>
-  }
 
+  getMovies = async () => {
+      // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json") 
+      // console.log(movies.data.data.movies) // ES6스럽지않은 방식. 하단 방식으로 개선 
+      const { 
+        data: { 
+          data: { movies }  
+        } 
+      } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+      //console.log(movies);
+      this.setState({ movies , isLoading: false}); // movies: movies 
+  };
+
+  componentDidMount(){
+    // setTimeout(() => {
+    //   this.setState({ isLoading: false });
+    // }, 3000);
+      this.getMovies();
+  } 
+
+  render(){
+    const { isLoading, movies }  = this.state;
+    return <div>{ isLoading ? "Loading.." : movies.map(movie => {
+      return <Movie key={ movie.id } id={ movie.id } year={ movie.year } title={ movie.title } summary={ movie.summary } poster={ movie.poster }/>
+    })}</div>
+  }
 }
 
 export default App;
