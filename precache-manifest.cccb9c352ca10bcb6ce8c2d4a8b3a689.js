e41fc9cf08d85d814c9ea485b6037b8ad887612d
jang-yunmi <newapache@naver.com>
2020-09-27 23:43:37 +0900

    Updates

---
+++ precache-manifest.cccb9c352ca10bcb6ce8c2d4a8b3a689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f5858e6da17aa7616bb8bd5c1429e9",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "c13f6590ff6a55ec0482",
+    "url": "/movie-app/static/css/main.fa26f003.chunk.css"
+  },
+  {
+    "revision": "5300f3a03d4b174dc2eb",
+    "url": "/movie-app/static/js/2.c2bad000.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.c2bad000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13f6590ff6a55ec0482",
+    "url": "/movie-app/static/js/main.6cdf01a8.chunk.js"
+  },
+  {
+    "revision": "daeaf96d14270f7d7af2",
+    "url": "/movie-app/static/js/runtime-main.2be5c0aa.js"
+  }
+]);
